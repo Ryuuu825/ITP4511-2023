f17bc8ee3a09d3ace54d4a6a1016e329b22c464f
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2023-04-27 23:53:49 +0800

    won't show internal server error

--- ITP4511_Assignment/src/java/ict/tag/GuestTag.java
+++ ITP4511_Assignment/src/java/ict/tag/GuestTag.java
@@ -50,18 +50,23 @@ public class GuestTag extends SimpleTagSupport {
             out.print("</tr>");
             out.print("</thead>");
             out.print("<tbody>");
-            for (int i = 1; i <= guests.getGuests().size(); i++) {
-                Guest guest = guests.getGuests().get(i - 1);
-                out.print("<tr class=\"align-middle\">");
-                out.print("<th scope = \"row\">" + i + "</th>");
-                out.print("<td>" + guest.getName() + "</td>");
-                out.print("<td>" + guest.getEmail() + "</td>");
-                if ("Member".equalsIgnoreCase(role)) {
-                    out.print("<td><a class=\"btn btn-primary btn-rounded me-2 btn-sm\" role=\"button\"\" href=\"editGuest?action=edit&bookingId=" + guests.getBookingId() + "&venueId=" + guests.getVenueId() + "&guestId=" + guest.getId() + "\">Edit</a>"
-                            + "<a class=\"btn btn-danger btn-rounded btn-sm\" role=\"button\" href=\"delGuest?action=delete&bookingId=" + guests.getBookingId() + "&venueId=" + guests.getVenueId() + "&guestId=" + guest.getId() + "\">Delete</a></td>");
+            try {
+                for (int i = 1; i <= guests.getGuests().size(); i++) {
+                    Guest guest = guests.getGuests().get(i - 1);
+                    out.print("<tr class=\"align-middle\">");
+                    out.print("<th scope = \"row\">" + i + "</th>");
+                    out.print("<td>" + guest.getName() + "</td>");
+                    out.print("<td>" + guest.getEmail() + "</td>");
+                    if ("Member".equalsIgnoreCase(role)) {
+                        out.print("<td><a class=\"btn btn-primary btn-rounded me-2 btn-sm\" role=\"button\"\" href=\"editGuest?action=edit&bookingId=" + guests.getBookingId() + "&venueId=" + guests.getVenueId() + "&guestId=" + guest.getId() + "\">Edit</a>"
+                                + "<a class=\"btn btn-danger btn-rounded btn-sm\" role=\"button\" href=\"delGuest?action=delete&bookingId=" + guests.getBookingId() + "&venueId=" + guests.getVenueId() + "&guestId=" + guest.getId() + "\">Delete</a></td>");
+                    }
+                    out.print("</tr>");
                 }
-                out.print("</tr>");
+            } catch (Exception e) {
+                System.out.println("Error generating prime: " + e);
             }
+
             out.print("</tbody></table>");
 
         } catch (IOException ioe) {
